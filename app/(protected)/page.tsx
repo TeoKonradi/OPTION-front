8f21b635d91da1fdc1637ff46551831b46c9d675
Teo Konradi <glpat-sGjoXtXCb4c76Pd9rQAY>
2024-01-19 02:39:50 +0000

    set eslint

--- app/(protected)/page.tsx
+++ app/(protected)/page.tsx
@@ -1,16 +1,15 @@
 "use client";
 
-
-
 export default function Home() {
   // const { data: dataMe, isSuccess } = useMyProfile();
   // console.log("useMyProfile isSuccess - ", isSuccess);
   // console.log("dataMe - ", dataMe);
 
   return (
-    <div className="flex flex-col gap-3 pb-4 pr-6 text-4xl">
+    <div className="flex flex-col gap-3 pb-4 pr-6 text-4xl ">
       <div className="">Main Page</div>
-      <div className="gap-3 flex-col  pb-4"></div>
+
+      <div className="flex-col gap-3  pb-4"></div>
     </div>
   );
 }
